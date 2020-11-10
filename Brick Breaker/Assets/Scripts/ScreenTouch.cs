820bbe0c66629f11e1a8927d919be9db8e683fa9
oguzhan topal <oguzhan2142@hotmail.com>
2020-11-10 12:13:54 +0300

    plank control system chaged

--- Brick Breaker/Assets/Scripts/ScreenTouch.cs
+++ Brick Breaker/Assets/Scripts/ScreenTouch.cs
@@ -14,83 +14,89 @@ public class ScreenTouch : MonoBehaviour
 
 
 
-    private float power;
 
-    Vector3 first;
-    Vector3 offset = Vector3.zero;
-    Vector3 plankScreen = Vector3.zero;
 
-    float witdh;
+
+
+
+
+    private float speed = 8f;
+
     private void Start()
     {
-        witdh = (Camera.main.WorldToViewportPoint(plank.transform.position) - Camera.main.WorldToViewportPoint(plank.leftEnd.position)).x;
+
 
     }
 
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            first = Input.mousePosition;
-            plankScreen = Camera.main.WorldToScreenPoint(plank.transform.position);
-            // Sag sol kaydirma
-            if (plank.movable)
-            {
-                offset = plank.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, plankScreen.z));
-            }
-
-
-        }
-
         if (Input.GetMouseButton(0))
         {
-            Vector3 last = Input.mousePosition;
 
-            // sag sol kaydirma
-            if (plank.movable)
-            {
+            if (!plank.movable)
+                return;
+                
+            Vector3 plankScreen = Camera.main.WorldToScreenPoint(plank.transform.position);
 
-                Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, plankScreen.z);
-                Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-
-                Vector3 plankLeftEndScreen = Camera.main.WorldToScreenPoint(plank.leftEnd.position);
-                Vector3 plankRightEndScreen = Camera.main.WorldToScreenPoint(plank.rightEnd.position);
-
-
-
-
-
-                if (plankLeftEndScreen.x < 0)
-                {
-
-
-                    if (first.x > last.x)
-                    {
-                        return;
-                    }
-                }
-
-                if (plankRightEndScreen.x > Screen.width)
-                {
-                    if (first.x < last.x)
-                    {
-                        return;
-                    }
-                }
-
-
-                plank.transform.position = new Vector3(curPosition.x, plank.transform.position.y, plank.transform.position.z);
-
-
-
-
-            }
-
+            Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, plankScreen.y, plankScreen.z));
+            // lerp and set the position of the current object to that of the touch, but smoothly over time.
+            plank.transform.position = Vector3.Lerp(plank.transform.position, touchedPos, Time.deltaTime * speed);
         }
-
 
 
 
     }
+
+    // private void oldControls()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         // Sag sol kaydirma
+    //         if (plank.movable)
+    //         {
+    //             plankScreen = Camera.main.WorldToScreenPoint(plank.transform.position);
+    //             offset = plank.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, plankScreen.z));
+    //         }
+    //     }
+    //     if (Input.GetMouseButton(0))
+    //     {
+
+    //         // sag sol kaydirma
+    //         if (plank.movable)
+    //         {
+    //             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, plankScreen.z);
+
+    //             Vector3 worldPoint = Camera.main.ScreenToWorldPoint(curScreenPoint);
+
+    //             Vector3 curPosition = worldPoint + offset;
+
+    //             plank.transform.position = new Vector3(curPosition.x * sensivity, plank.transform.position.y, plank.transform.position.z);
+
+    //         }
+
+    //     }
+    // }
+
+
+
+    // private void goToTouchedPos()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         plankScreen = Camera.main.WorldToScreenPoint(plank.transform.position);
+    //         Vector3 input = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, plankScreen.z));
+
+    //         destination = new Vector3(input.x, yPos, zPos);
+
+    //     }
+
+    //     plank.transform.position = Vector3.MoveTowards(plank.transform.position, destination, speed * Time.deltaTime);
+    // }
+
+
+
+
+
+
 }
